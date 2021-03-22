94cc15291a9b3ddaadd1167d10a637430a805c34
Vinicius Rodrigues <vrsstudy@gmail.com>
2021-03-22 08:59:53 -0400

    first commit

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -3,22 +3,11 @@ import { container } from 'tsyringe';
 import '@modules/users/providers';
 import './providers';
 
-import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
-import AppointmentsRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository';
-
 import IUserRepository from '@modules/users/repositories/IUserRepository';
 import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
 
 import IUserTokensRepository from '@modules/users/repositories/IUserTokensRepository';
 import UsersTokensRepository from '@modules/users/infra/typeorm/repositories/UserTokensRepository';
-
-import INotificationsRepository from '@modules/notifications/repositories/INotificationsRepository';
-import NotificationsRepository from '@modules/notifications/infra/typeorm/repositories/NotificationsRepository';
-
-container.registerSingleton<IAppointmentsRepository>(
-  'AppointmentsRepository',
-  AppointmentsRepository,
-);
 
 container.registerSingleton<IUserRepository>(
   'UsersRepository',
@@ -28,9 +17,4 @@ container.registerSingleton<IUserRepository>(
 container.registerSingleton<IUserTokensRepository>(
   'UserTokensRepository',
   UsersTokensRepository,
-);
-
-container.registerSingleton<INotificationsRepository>(
-  'NotificationsRepository',
-  NotificationsRepository,
 );
--- src/shared/container/providers/MailTemplateProvider/fakes/FakeMailTemplateProvider.ts
+++
@@ -1,9 +0,0 @@
-import IMailTemplateProvider from '../models/IMailTemplateProvider';
-
-class FakeMailProvider implements IMailTemplateProvider {
-  public async parse(): Promise<string> {
-    return 'Mail Content';
-  }
-}
-
-export default FakeMailProvider;
